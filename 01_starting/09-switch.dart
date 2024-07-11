6c9719f93a1a1a40272adf397926e720241f8a7a
SJGDFPRGVW259\FORMACION <vessionmodel@gamail.com>
2024-07-11 14:53:04 -0500

    add loops folder

---
+++ 01_starting/09-switch.dart
@@ -0,0 +1,19 @@
+void main(){
+  //SWITCH AND CASE STATEMENTS
+
+  String grade = 'R'; 
+
+  switch(grade){
+
+    case 'A':
+          print('A excelent grade');
+          break; 
+    case 'B': 
+          print('Good job'); 
+          break; 
+    case  'R': 
+          print('Reprobated');
+          break;  
+  }
+
+}
