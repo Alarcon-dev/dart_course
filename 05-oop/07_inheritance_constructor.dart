5e94a45250e8df9acd19c44aa180fce4249ce258
SJGDFPRGVW259\FORMACION <vessionmodel@gamail.com>
2024-07-17 09:13:02 -0500

    contructor and named constructor within inheritance

--- 05-oop/07_inheritance_constructor.dart
+++ 05-oop/07_inheritance_constructor.dart
@@ -3,6 +3,12 @@
  */
 void main() {
 
+  var dog1 = Dog('pug');
+  print(dog1);
+  print(''); 
+
+  var dog2 = Dog('labrador');
+  print(dog2);
 
 }
 
@@ -10,10 +16,25 @@ class Animal {
 
   String color = ''; 
 
+  Animal( String color){
+    this.color = color;
+    print(this.color);
+  }
+
+  Animal.animalConstructorOne( String color ){
+    this.color = color; 
+    print(this.color);
+  }
+
 }
 
 class Dog extends Animal {
 
   String breed = '';
+
+  Dog(String breed): super.animalConstructorOne('Green'){
+    this.breed = breed;
+    print(this.breed);
+  }
 
 }
