30f7e3594a83f755a6960c07615488d37f0cb361
SJGDFPRGVW259\FORMACION <vessionmodel@gamail.com>
2024-07-17 17:31:36 -0500

    Higer and order functions

--- 06_expresions/01_lambda.dart
+++ 06_expresions/01_lambda.dart
@@ -1,10 +1,12 @@
-
 void main(){
- 
-   print('Hola');
+
+  myLambda(3,2);
+  print(showSum(4,5));
+
 }
 
-
+Function myLambda = (int a , int b) => print(a+b); 
+var showSum = ( int a , int b ) => a + b; 
 
 
 
