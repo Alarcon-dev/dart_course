93e8895a33bed982a920649ed9334fdf93d4ae97
SJGDFPRGVW259\FORMACION <vessionmodel@gamail.com>
2024-07-17 15:26:37 -0500

    Continue with expresions

---
+++ 05-oop/10_static_method_variables.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  // se puded acceder a las propiedades o los metodos de la clase sin 
+  // instanciar la clase, el porble es que se pueden alterar los valores de 
+  // la variables.
+  print(Circle.pi);
+
+}
+
+
+class Circle {
+
+  static double pi = 14.16; 
+
+
+}
